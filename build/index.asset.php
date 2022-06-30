b08b7f40141e165d6a54985b980574811a46c7eb
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-06-30 10:48:24 +0200

    added block support for colors, typography and spacing settings

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd4f4134c252551f92835');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '92f96a913c2fe72e5807');
